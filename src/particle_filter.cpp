73bc97824c84b1094f2b5c211c4186257becf32b
Doug Guastaferro <dguastaferro@gmail.com>
2017-05-22 22:19:28 -0700

    update weight step implemented

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -9,8 +9,10 @@
 #include <algorithm>
 #include <iostream>
 #include <numeric>
+#include <limits>
 
 #include "particle_filter.h"
+#include "helper_functions.h"
 
 using namespace std;
 
@@ -20,7 +22,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 	// Add random Gaussian noise to each particle.
 	// NOTE: Consult particle_filter.h for more information about this method (and others in this file).
   
-  num_particles = 50;
+  num_particles = 10;
   
   double std_x = std[0];
   double std_y = std[1];
@@ -96,7 +98,24 @@ void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::ve
 	//   observed measurement to this particular landmark.
 	// NOTE: this method will NOT be called by the grading code. But you will probably find it useful to 
 	//   implement this method and use it as a helper during the updateWeights phase.
-
+  
+  for (int i = 0; i < observations.size(); i++) {
+    LandmarkObs obs = observations[i];
+    double x1 = obs.x;
+    double y1 = obs.y;
+    double min = numeric_limits<double>::max();
+    for (int j = 0; j < predicted.size(); j++) {
+      LandmarkObs pred = predicted[j];
+      double x2 = pred.x;
+      double y2 = pred.y;
+      
+      double distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+      if (distance < min){
+        obs.id = pred.id;
+      }
+    }
+    observations[i] = obs;
+  }
 }
 
 void ParticleFilter::updateWeights(double sensor_range, double std_landmark[], 
@@ -112,6 +131,50 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 	//   3.33. Note that you'll need to switch the minus sign in that equation to a plus to account 
 	//   for the fact that the map's y-axis actually points downwards.)
 	//   http://planning.cs.uiuc.edu/node99.html
+  
+  /*
+   * 1. Take vehicle observations and translate each into map coordinates
+   * 2. Look for the nearest landmark to the transformed observation
+   * 3. Calculate the probability using the multi-variate gaussian dist
+   * 4. Repeat 1-3 for each observation
+   * 5. Multiply each of the probabilities together - this is the new weight
+   */
+  
+  for (Particle& p : particles) {
+    
+    double weight = 1;
+    
+    for (LandmarkObs obs : observations) {
+      // Translate vehicle observation coordinates into map coordinates  
+      double map_x = obs.x * cos(p.theta) - obs.y * sin(p.theta) + p.x;
+      double map_y = obs.x * sin(p.theta) + obs.y * cos(p.theta) + p.y;
+      
+      // look for nearest landmark to the transformed observation
+      double min_dist = numeric_limits<double>::max();
+      Map::single_landmark_s nearest_landmark;
+      for (Map::single_landmark_s landmark : map_landmarks.landmark_list) {
+        double distance = dist(landmark.x_f, landmark.y_f, map_x, map_y);
+        if (distance < min_dist) {
+          min_dist = distance;
+          nearest_landmark = landmark;
+        }
+      }
+      
+      //Compute multi-variate Gaussian distribution
+      double sig_x = std_landmark[0];
+      double sig_y = std_landmark[1];
+      double dividend = 2. * M_PI * sig_x * sig_y;
+      double x_term = pow(obs.x - nearest_landmark.x_f, 2) / (2. * pow(sig_x, 2));
+      double y_term = pow(obs.y - nearest_landmark.y_f, 2) / (2. * pow(sig_y, 2));
+      
+      double prob = 1/dividend * exp(-(x_term + y_term));
+      weight *= prob;
+      
+    }
+    
+    p.weight = weight;
+  }
+  
 }
 
 void ParticleFilter::resample() {
